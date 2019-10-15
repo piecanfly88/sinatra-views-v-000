6b1cf02f5f21c5c396d48b57bed431f439279878
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-10-15 18:20:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	# get '/' do
-	# 	erb :index
-	# end
-	#
+	get '/' do
+		erb :index
+	end
+	
 	#  get "/info" do
 	# 	 erb :info
 	#  end
